99ba01ee05a8d442b0e4e3eeb59d258420578a28
hituzi ando <hhh.ando@gmail.com>
2022-06-02 18:26:29 +0900

    Added macOS architecture

---
+++ HTZSequencer macOS/HTZSequencer_macOS.h
@@ -0,0 +1,19 @@
+//
+//  HTZSequencer_macOS.h
+//  HTZSequencer macOS
+//
+//  Created by Masaki Ando on 2022/06/02.
+//  Copyright © 2022 Hituzi Ando. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HTZSequencer_macOS.
+FOUNDATION_EXPORT double HTZSequencer_macOSVersionNumber;
+
+//! Project version string for HTZSequencer_macOS.
+FOUNDATION_EXPORT const unsigned char HTZSequencer_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTZSequencer_macOS/PublicHeader.h>
+
+
